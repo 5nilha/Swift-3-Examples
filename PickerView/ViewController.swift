e9d88c824071224fe24d072767ec38127078ecf9
5nilha <bwtdirect@gmail.com>
2017-01-02 02:28:47 -0500

    Completed Pickerview example

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    @IBOutlet weak var label: UILabel!
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    // We shold create the array with all itensthat will show in the pickerview
+    
+    
+    let foods = ["Apples", "Bananas", "Corn", "Beans", "Tomatoes"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
+        
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+        //Define how many sections we have in one row
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return foods[row]
+        
+        //Define what text should be in each row
     }
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return foods.count
+         //Define how many rows we want
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        label.text = foods[row]
+        // Display the selected row in our label
+    }
+    
+   
 
 }
 
